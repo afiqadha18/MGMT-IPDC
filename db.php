d8957e9b94e2d0340a3bd944aa7ce023b00803d2
afiqadha18 <afiqadhabaharudin@gmail.com>
2022-10-21 10:59:13 +0800

    update db config

--- db.php
+++ db.php
@@ -11,13 +11,13 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT nasname, shortname, type FROM nas ORDER BY uid";
+$sql = "SELECT nasname, shortname FROM nas ORDER BY id";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo $row["uid"]. " - Router IP: " . $row["nasname"]. " | Hostname: " . $row["shortname"] . " | Type: " . $row["type"] . "<br>";
+    echo $row["id"]. " - Router IP: " . $row["nasname"]. " | Hostname: " . $row["shortname"] . "<br>";
   }
 } else {
   echo "0 results <br>";
