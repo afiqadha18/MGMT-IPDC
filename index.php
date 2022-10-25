3772fb70578912df62995dfb338dc5e133d9e58d
afiqadha18 <afiqadhabaharudin@gmail.com>
2022-10-25 17:59:56 +0800

    redesign all

--- index.php
+++ index.php
@@ -3,28 +3,144 @@
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
         <title>IPDC Management Hub</title>
-        <link rel="stylesheet" href="/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="/css/custom-style.css" />
+        <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="./css/style.css" />
     </head>
     <body>
-        <ul class="nav justify-content-center">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">About</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="./radius.php">Radius</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Ansible</a>
-            </li>
-        </ul>
+        <div class="wrapper d-flex align-items-stretch">
+            <nav id="sidebar">
+                <div class="p-4 pt-5">
+                    <a href="#" class="img logo rounded-circle mb-5" style="background-image: url(images/logo.jpg);"></a>
+                    <ul class="list-unstyled components mb-5">
+                        <li class="active">
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                        <a href="#EquipmentListSub" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Equipment Management</a>
+                            <ul class="collapse list-unstyled" id="EquipmentListSub">
+                                <li>
+                                    <a href="#">Hong Kong SC</a>
+                                </li>
+                                <li>
+                                    <a href="#">Singapore SC</a>
+                                </li>
+                                <li>
+                                    <a href="#">Taiwan SC</a>
+                                </li>
+                                <li>
+                                    <a href="#">Ashburn USA SC</a>
+                                </li>
+                                <li>
+                                    <a href="#">Frankfurt SC</a>
+                                </li>
+                                <li>
+                                    <a href="#">Equipment Group</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#RadiusSub" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Radius Accounting</a>
+                            <ul class="collapse list-unstyled" id="RadiusSub">
+                                <li>
+                                    <a href="#">Manage Users</a>
+                                </li>
+                                <li>
+                                    <a href="#">by Equipment Group</a>
+                                </li>
+                                <li>
+                                    <a href="#">by Scrubbing Centre</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                        <a href="#EquipmentBackupSub" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Equipment Backup</a>
+                            <ul class="collapse list-unstyled" id="EquipmentBackupSub">
+                                <li>
+                                    <a href="#">Hong Kong SC</a>
+                                </li>
+                                <li>
+                                    <a href="#">Singapore SC</a>
+                                </li>
+                                <li>
+                                    <a href="#">Taiwan SC</a>
+                                </li>
+                                <li>
+                                    <a href="#">Ashburn USA SC</a>
+                                </li>
+                                <li>
+                                    <a href="#">Frankfurt SC</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                        <a href="#MyAccountSub" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">My Account</a>
+                            <ul class="collapse list-unstyled" id="MyAccountSub">
+                                <li>
+                                    <a href="#">Profile</a>
+                                </li>
+                                <li>
+                                    <a href="#">Log Out</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <div class="footer">
+                        <p>
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i><br> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a> <br> for <a href="https://www.ipdc.asia/" target="_blank"> IPDC Solutions Pte Ltd. </a>
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        </p>
+                    </div>
+                </div>
+            </nav>
+            <!-- Page Content  -->
+            <div id="content" class="p-4 p-md-5">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="container-fluid">
+                        <button type="button" id="sidebarCollapse" class="btn btn-primary">
+                        <i class="fa fa-bars"></i>
+                        <span class="sr-only">Toggle Menu</span>
+                        </button>
+                        <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fa fa-bars"></i>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="nav navbar-nav ml-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Equipment Management</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Radius Accounting</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Equipment Backup</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Log Out</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <h2 class="mb-4">Home</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
+        <script src="./js/jquery.min.js"></script>
+        <script src="./js/popper.js"></script>
+        <script src="./js/bootstrap.min.js"></script>
+        <script src="./js/main.js"></script>
         <?php
             // date_default_timezone_set("Asia/Singapore");
             // echo date('d-M-Y H:i:s', time());
             
             ?>
     </body>
-    <script src="./js/bootstrap.min.js"></script>
 </html>
